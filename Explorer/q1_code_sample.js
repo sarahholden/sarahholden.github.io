1221f3885ca4bf4bbbb20002cb2923ccb3d0d22c
sarahholden <sarahbeth.holden@gmail.com>
2015-12-09 13:28:17 -0800

    updated q1

--- Explorer/q1_code_sample.js
+++ Explorer/q1_code_sample.js
@@ -1,9 +1,7 @@
 $(window).on('scroll', function() {
+	var $socialLinkTab = $(".social-link-tab"),
+	    articleListOffset = $('#article-list').offset().top;
 
-var $socialLinkTab = $(".social-link-tab"),
-    leadStoryHeight = $('.lead-story').outerHeight();
-
-	var scrollTop = $(this).scrollTop();
   if( __________________________ ) {
     $socialLinkTab.addClass('scrolled');
   } else {
