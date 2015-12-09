3b6882ea19c07e8a776d767232cb7f16221a64d7
sarahholden <sarahbeth.holden@gmail.com>
2015-12-09 14:16:19 -0800

    q3 code samples

--- Explorer/js/carousel.js
+++ Explorer/js/carousel.js
@@ -4,14 +4,12 @@
 
 	"use strict"
 
+	$('#prev').prop('disabled', true);
+
 	var images = ['images/gallery_1.jpeg', 'images/gallery_2.jpeg', 'images/gallery_3.jpeg', 'images/gallery_4.jpeg', 'images/gallery_5.jpeg', 'images/gallery_6.jpeg'];
 
 	var currentPosition = 0;
 
-
-	// EVENTS ---------
-
-	// Listen for click events on the next and previous buttons
 	$('#next').on('click', nextImage);
 	$('#prev').on('click', previousImage);
 
@@ -21,39 +19,34 @@
 	function nextImage(e){
 		e.preventDefault();
 
-		// If the currentPosition is at the last image
-	 	if (currentPosition === images.length - 1) {
-	 		// Disable the next button
-			currentPosition = 0;
-			// Calculate votes
-		} else {
-			// Update the current position
-			currentPosition += 1;
-		}
+		currentPosition += 1;
 
-		// Update source
-		changeImage();
+		$('#prev').prop('disabled', false);
+
+		// ___________________________________ (blank here)
+		$('#galleryImage').attr('src', images[currentPosition]);
+
+	 	if (currentPosition === images.length - 1) {
+
+			$('#next').prop('disabled', true);
+
+		}
 	}
 
 	function previousImage(e){
 		e.preventDefault();
 
-		// If the currentPosition is at the first item (the zero index), disable the previous button
-		if (currentPosition === 0 ) {
-			currentPosition = images.length - 1;
-		} else {
-			// Update the current position
-			currentPosition -= 1;
-		}
+		currentPosition -= 1;
 
-		// Update the source
-		changeImage();
-	}
+		$('#next').prop('disabled', false);
 
-	function changeImage(){
-		// Update the src attribute to the urls that's stored at the currentPosition in the array
 		$('#galleryImage').attr('src', images[currentPosition]);
 
+		if (currentPosition === 0 ) {
+			$('#prev').prop('disabled', true);
+		}
 	}
+
+
 
 })();
