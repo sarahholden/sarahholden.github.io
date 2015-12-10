b19e13f4e96de5fe4fd933315518a3f4b4ca5546
sarahholden <sarahbeth.holden@gmail.com>
2015-12-09 16:12:13 -0800

    challenge version

--- Explorer/js/carousel.js
+++ Explorer/js/carousel.js
@@ -2,8 +2,6 @@
 
 	"use strict"
 
-	$('#prev').prop('disabled', true);
-
 	var images = ['images/gallery_1.jpeg', 'images/gallery_2.jpeg', 'images/gallery_3.jpeg', 'images/gallery_4.jpeg', 'images/gallery_5.jpeg', 'images/gallery_6.jpeg'];
 
 	var currentPosition = 0;
@@ -17,15 +15,8 @@
 
 		currentPosition += 1;
 
-		$('#prev').prop('disabled', false);
-
 		$('#galleryImage').attr('src', images[currentPosition]);
 
-	 	if (currentPosition === images.length - 1) {
-
-			$('#next').prop('disabled', true);
-
-		}
 	}
 
 	function previousImage(e){
@@ -33,13 +24,8 @@
 
 		currentPosition -= 1;
 
-		$('#next').prop('disabled', false);
-
 		$('#galleryImage').attr('src', images[currentPosition]);
 
-		if (currentPosition === 0 ) {
-			$('#prev').prop('disabled', true);
-		}
 	}
 
 
--- Explorer/js/main.js
+++ Explorer/js/main.js
@@ -2,47 +2,6 @@
 
 	"use strict";
 
-	// INTERACTIVE NAV BAR
-
-	// When the user clicks on a link in the main nav
-	$('#main-nav a').on('click', function (e) {
-
-		// Prevent the default action
-		e.preventDefault();
-
-		// If the item the user clicked already has the active class
-		if ($(this).hasClass('active')) {
-
-			// Remove the active class
-			$(this).removeClass('active');
-
-			// Slide up the dropdown wrapper
-			$('#dropdown').slideUp(500);
-
-		} else {
-			// Find out what section the user wants to show
-			var sectionToShow = $(this).data('related-panel');
-
-			// Remove active class from all other nav items
-			$('.active').removeClass('active');
-
-			// Add the active class to the current item
-			$(this).addClass('active');
-
-			// Slide down the dropdown wrapper
-			$('#dropdown').slideDown(400);
-
-			// Hide all other nav panels
-			$('.nav-panel').hide();
-
-			// Show the related nav panel
-			$('#' + sectionToShow).fadeIn(400);
-		}
-
-	});
-
-
-
 	$(window).on('scroll', function() {
 		var $socialLinkTab = $(".social-link-tab");
 		var articleListOffset = $('#article-list').offset().top;
@@ -54,6 +13,10 @@
 	    $socialLinkTab.removeClass('scrolled');
 	  }
 	});
+
+
+	// Write your code here
+	
 
 
 })();
