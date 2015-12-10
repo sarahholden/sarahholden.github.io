568d7acc23af4845e738a5b27e5c3502ce918e87
sarahholden <sarahbeth.holden@gmail.com>
2015-12-09 16:09:52 -0800

    updated readme

--- Explorer/js/carousel.js
+++ Explorer/js/carousel.js
@@ -2,6 +2,8 @@
 
 	"use strict"
 
+	$('#prev').prop('disabled', true);
+
 	var images = ['images/gallery_1.jpeg', 'images/gallery_2.jpeg', 'images/gallery_3.jpeg', 'images/gallery_4.jpeg', 'images/gallery_5.jpeg', 'images/gallery_6.jpeg'];
 
 	var currentPosition = 0;
@@ -15,8 +17,15 @@
 
 		currentPosition += 1;
 
+		$('#prev').prop('disabled', false);
+
 		$('#galleryImage').attr('src', images[currentPosition]);
 
+	 	if (currentPosition === images.length - 1) {
+
+			$('#next').prop('disabled', true);
+
+		}
 	}
 
 	function previousImage(e){
@@ -24,8 +33,13 @@
 
 		currentPosition -= 1;
 
+		$('#next').prop('disabled', false);
+
 		$('#galleryImage').attr('src', images[currentPosition]);
 
+		if (currentPosition === 0 ) {
+			$('#prev').prop('disabled', true);
+		}
 	}
 
 
